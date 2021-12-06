349051f4bb215ebb3312f661fc88e9418ce85d2b
Paul Dämmig <Paul-Daemmig@gmx.net>
2021-12-06 19:06:47 +0100

    Emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -44,6 +44,7 @@
    :config (rainbow-delimiters-mode))
 
 (use-package magit)
+(use-package projectile)
  ;;----------------------------------------------
  (use-package doom-themes
    :init (load-theme 'doom-palenight t))
@@ -51,13 +52,20 @@
  (use-package all-the-icons)
  (use-package doom-modeline
    :init (doom-modeline-mode 1))
+
+ ;;---------------------------------------------
+ (use-package eglot)
+ (use-package company
+   :config (global-company-mode t))
+ (use-package yasnippet
+   :config (yas-global-mode 1))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(magit use-package rainbow-delimiters evil doom-themes doom-modeline counsel)))
+   '(vterm yasnippet use-package rainbow-delimiters magit evil eglot doom-themes doom-modeline counsel company)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
